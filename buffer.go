3bff8680ce61a5298f09f8b8ba2812580cff41fe
Ключников Валерий <valeriy.v.klyuchnikov@gmail.com>
2021-12-09 23:08:59 +0300

    Channels for events + some fixes

--- buffer.go
+++ buffer.go
@@ -1,22 +1,37 @@
 package buffer
 
 import (
+	"context"
+	"log"
 	"strings"
 
 	"github.com/KlyuchnikovV/buffer/cursor"
+	"github.com/KlyuchnikovV/edigode/types"
 	"github.com/KlyuchnikovV/gapbuf"
 	"github.com/KlyuchnikovV/linetree"
 	"github.com/KlyuchnikovV/linetree/node"
+	"github.com/wailsapp/wails"
 )
 
 type Buffer struct {
+	types.CancellableContext
+	runtime *wails.Runtime
+
+	keyboardEvents chan types.KeyboardEvent
+	selectEvents   chan types.SelectEvent
+
 	cursor.Cursors
 }
 
-func New(str ...byte) *Buffer {
+func New(ctx context.Context, str ...byte) *Buffer {
 	var (
 		lines  = strings.Split(string(str), "\n")
 		buffer = &Buffer{
+			CancellableContext: *types.NewCancellableContext(ctx),
+
+			keyboardEvents: make(chan types.KeyboardEvent, 100), //TODO: length of keyboard chan
+			selectEvents:   make(chan types.SelectEvent, 100),
+
 			Cursors: *cursor.New(
 				linetree.New(
 					node.New(*gapbuf.New()),
@@ -47,22 +62,107 @@ func New(str ...byte) *Buffer {
 	return buffer
 }
 
+func (buffer *Buffer) WailsInit(runtime *wails.Runtime) error {
+	buffer.runtime = runtime
+
+	return nil
+}
+
+func (buffer *Buffer) Start() error {
+	if err := buffer.CancellableContext.Start(); err != nil {
+		return err
+	}
+
+	go buffer.listenEvents()
+
+	return nil
+}
+
+func (buffer *Buffer) KeyboardEvents() chan types.KeyboardEvent {
+	return buffer.keyboardEvents
+}
+
+func (buffer *Buffer) SelectEvents() chan types.SelectEvent {
+	return buffer.selectEvents
+}
+
+func (buffer *Buffer) listenEvents() {
+	for {
+		select {
+		case event, ok := <-buffer.keyboardEvents:
+			if !ok {
+				panic("!ok")
+			}
+			if err := buffer.HandleKeyboardEvent(event); err != nil {
+				panic(err)
+			}
+		case event, ok := <-buffer.selectEvents:
+			if !ok {
+				panic("!ok")
+			}
+			if err := buffer.HandleSelect(event); err != nil {
+				panic(err)
+			}
+		case <-buffer.CancellableContext.Done():
+			if err := buffer.CancellableContext.Cancel(); err != nil {
+				panic(err) //TODO: errors channel
+			}
+		}
+	}
+}
+
+func (buffer *Buffer) HandleKeyboardEvent(event types.KeyboardEvent) error {
+	log.Printf("TRACE: %s", event)
+
+	switch {
+	case event.Key == "Delete":
+	case event.Key == "Backspace":
+		buffer.Delete()
+	case event.Key == "Enter":
+		buffer.NewLine()
+	case event.Key == "Tab":
+		buffer.Insert('\t')
+	default:
+		buffer.Insert([]byte(event.Key)...)
+	}
+
+	buffer.runtime.Events.Emit("buffer_changed" + event.Buffer)
+
+	return nil
+}
+
+func (buffer *Buffer) HandleSelect(event types.SelectEvent) error {
+	log.Printf("SELECT: %s", event)
+
+	var line, offset = buffer.translateOffset(event.Symbol)
+	log.Printf("SELECT: line %d, offset %d (symbol %d)", line, offset, event.Symbol)
+
+	buffer.SetCursor(line, offset)
+
+	buffer.runtime.Events.Emit("cursor_changed", event.Buffer)
+
+	return nil
+}
+
 func (buffer *Buffer) NewLine(bytes ...byte) {
 	buffer.ApplyToAll(func(gapBuffer *gapbuf.GapBuffer, lines *[]int, index int) {
 		(*lines)[index]++
 
-		if err := buffer.Tree.Insert((*lines)[index],
-			*node.New(
-				*gapbuf.New(append(gapBuffer.Split(), bytes...)...),
-			),
-		); err != nil {
+		var (
+			data = gapBuffer.Split()
+			text = append(data, bytes...)
+			buf  = *gapbuf.New(text...)
+			node = *node.New(buf)
+		)
+
+		if err := buffer.Tree.Insert((*lines)[index], node); err != nil {
 			panic(err)
 		}
 	})
 }
 
 func (buffer *Buffer) SetCursor(line, offset int) {
-	if line != -1 {
+	if line > 0 && line < buffer.Tree.Size() {
 		buffer.Cursors.Lines[0] = line
 	}
 
@@ -81,11 +181,11 @@ func (buffer *Buffer) Insert(bytes ...byte) {
 
 func (buffer *Buffer) Delete() {
 	buffer.ApplyToAll(func(gapBuffer *gapbuf.GapBuffer, lines *[]int, index int) {
-		if gapBuffer.Offset() != 0 {
+		if gapBuffer.GetCursor() != 0 {
 			gapBuffer.Delete()
 			return
 		}
-		if (*lines)[index] == 0 {
+		if (*lines)[index] == 1 {
 			return
 		}
 
@@ -108,7 +208,7 @@ func (buffer *Buffer) Delete() {
 	})
 }
 
-func (buffer *Buffer) TranslateOffset(offset int) (int, int) {
+func (buffer *Buffer) translateOffset(offset int) (int, int) {
 	var (
 		data      = buffer.Tree.String()[:offset]
 		line      = strings.Count(data, "\n") + 1
@@ -120,4 +220,17 @@ func (buffer *Buffer) TranslateOffset(offset int) (int, int) {
 	}
 
 	return line, len(data[lastIndex+1:])
+}
+
+func (buffer *Buffer) GetLines() ([]string, error) {
+	nodes, err := buffer.Tree.GetRange(1, buffer.Tree.Size())
+	if err != nil {
+		return nil, err
+	}
+	var result = make([]string, len(nodes))
+	for i, node := range nodes {
+		result[i] = node.Line.String()
+		result[i] = strings.ReplaceAll(result[i], " ", "\u00A0")
+	}
+	return result, nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/KlyuchnikovV/buffer
 go 1.16
 
 require (
+	github.com/KlyuchnikovV/edigode v0.0.0-20211209121658-42980a9c6cd7
 	github.com/KlyuchnikovV/gapbuf v0.0.0-20211209120053-9298a9e329ad
 	github.com/KlyuchnikovV/linetree v0.0.0-20211209120521-7dcdde3ffe70
 	github.com/davecgh/go-spew v1.1.1 // indirect
